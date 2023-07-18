3f4a9eacb0a6bcce4f7a4cecb4d60dbaf5925a64
Steve Shumaker <steve.j.shumaker@gmail.com>
2023-07-17 21:12:44 -0500

    added questions

---
+++ heroes_answers.sql
@@ -0,0 +1,19 @@
+-- 1. How many powers are there?
+
+-- 2. List all heroes and their powers.
+
+-- 3. Find all of Professor X's powers.
+
+-- 4. Add a new super power. 
+
+-- 5. Add this power to an existing hero.
+
+-- 6. Which heroes have laser vision? Include their name and alias.
+
+-- 7. How many can fly? A twist on the above...
+
+-- 8. What is the average super strength power level?
+
+-- 9. Show all the information for heroes with super strength power level over 2.
+
+-- 10. Find the average power level for all powers. Include the power name and descriptions.
